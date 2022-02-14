0d77b89f3024f54548598c5ada4884de92ab0179
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-02-13 21:11:55 -0300

    Fazendo primeiro select com JDBC

--- src/br/com/alura/testes/TestandoConexao.java
+++ src/br/com/alura/testes/TestandoConexao.java
@@ -2,6 +2,8 @@ package br.com.alura.testes;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class TestandoConexao {
 
@@ -10,7 +12,28 @@ public class TestandoConexao {
 		Connection connection = DriverManager
 				.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "97777");
 		
-		System.out.println("Conexão criada");
+		// STATEMENT É UMA INTERFACE UTILIZADA PARA EXECUTAR COMANDOS SQL
+		Statement stm = connection.createStatement();
+		
+		// A PARTIR DO STATEMENT É CRIADO O EXECUTE
+		stm.execute("select * from produto");
+		
+		// A PARTIR DO RESULTSET POSSO ACESSAR OS RESULTADOS
+		ResultSet resultSet = stm.getResultSet();
+		
+		while(resultSet.next()) {
+		
+			int id = resultSet.getInt("id");
+			System.out.println(id);
+			
+			String nome = resultSet.getString("nome");
+			System.out.println(nome);
+			
+			String descricao = resultSet.getString("descricao");
+			System.out.println(descricao);
+			
+			
+		}
 		
 		connection.close();
 
