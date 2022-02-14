2949b9f4453e5a090c99516646c46a656d767d65
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-02-14 14:53:59 -0300

    Configurando pull de conexões para o mySQL

--- src/br/com/alura/testes/ConnectionFactory.java
+++ src/br/com/alura/testes/ConnectionFactory.java
@@ -1,14 +1,35 @@
 package br.com.alura.testes;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+
+		// CADA BANCO TEM SEU DATASOURCE (INTERFACE PARA CRIAR UM POOL DE CONEXÕES)
+		// NESSE CASO NÃO CRIAMOS NA MÃO CADA CONXÃO COM O BANCO
+		// NO MYSQL UTILIZAMOS: ComboPooledDataSource PASSANDO A URL, USUÁRIO E SENHA.
+		
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("97777");
+
+		// FEITO ISSO DECLARAMOS QUE O DATASOURCE É ESSE ComboPooledDataSource
+		this.dataSource = comboPooledDataSource;
+
+	}
+
 	public Connection newConnection() throws Exception {
 
-		return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC",
-				"root", "97777");
+		// E A PARTIR DO DATASOURCE RECUPERAMOS A CONEXÃO
+		return dataSource.getConnection();
 
 	}
 
