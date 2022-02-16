e97810516f855e66adf82b32f673f2c24e243346
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-02-16 12:31:53 -0300

    Implementando inner join evitando querys N + 1

--- src/br/com/alura/model/Categoria.java
+++ src/br/com/alura/model/Categoria.java
@@ -1,9 +1,13 @@
 package br.com.alura.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Categoria {
 
 	private int id;
 	private String nome;
+	private List<Produto> produtos = new ArrayList<>();
 
 	public Categoria(int id, String nome) {
 		this.id = id;
@@ -12,7 +16,15 @@ public class Categoria {
 
 	@Override
 	public String toString() {
-		return "ID: " + this.id + " Nome: " + this.nome;
+		return "ID: " + this.id + " Nome: " + this.nome + " Produtos da categoria: " + this.produtos;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public String getNome() {
+		return nome;
 	}
 
 }
--- src/br/com/alura/testes/TestaListagemDeCategorias.java
+++ src/br/com/alura/testes/TestaListagemDeCategorias.java
@@ -2,23 +2,26 @@ package br.com.alura.testes;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import br.com.alura.dao.CategoriaDAO;
 import br.com.alura.dao.ConnectionFactory;
+import br.com.alura.model.Categoria;
 
 public class TestaListagemDeCategorias {
 
 	public static void main(String[] args) throws SQLException, Exception {
 
-		
-		try (Connection con = new ConnectionFactory().newConnection()){
-			
+		try (Connection con = new ConnectionFactory().newConnection()) {
+
 			CategoriaDAO dao = new CategoriaDAO(con);
-			
-			dao.listar().forEach(c -> System.out.println(c));
+
+			List<Categoria> listarComProdutos = dao.listarComProdutos();
+
+			listarComProdutos.forEach(c -> System.out.println(c));
 			
 		}
-		
+
 	}
 
 }
